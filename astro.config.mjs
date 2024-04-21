202da850ef2ad4db6fd616c9113ac3b5fc7002c8
caijinghao <fragtex@163.com>
2024-04-21 15:35:09 +0800

    feat:add astro hello world to github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://Fragtex254.github.io",
   base: "/",
   integrations: [
     tailwind(),
